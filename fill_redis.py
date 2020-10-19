99e1e9ebb7a52136ce2492e873ac3f918b0ecc13
José Andrés Ceballos Vadillo <andres@ceballos.dev>
2020-10-18 22:46:35 -0500

    Adding script to fill redis database

---
+++ fill_redis.py
@@ -0,0 +1,23 @@
+import redis
+import time 
+
+# connect to redis
+client = redis.Redis(
+        host = 'localhost',
+        port = 6379
+        )
+
+reg = 100_000
+percent = reg / 10
+
+start_time = time.time()
+print(f'Comenzando la carga de {reg} registros.')
+for n in range(reg):
+	client.set('key:'+str(n), 'value:'+str(n))
+	if(n % percent == 0):
+		seconds = (time.time() - start_time)
+		print(f'  {n/percent*10}% en: {seconds} segundos')
+
+seconds = (time.time() - start_time)
+print(f'{reg} registros en: {seconds} segundos\n')
+
