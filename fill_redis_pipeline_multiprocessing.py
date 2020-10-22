1d924d5fc17a8c0d63b95a01369f0acdf6190693
José Andrés Ceballos Vadillo <andres@ceballos.dev>
2020-10-21 21:41:16 -0500

    Improve multiprocessing

--- fill_redis_pipeline_multiprocessing.py
+++ fill_redis_pipeline_multiprocessing.py
@@ -2,26 +2,29 @@ import multiprocessing as mp
 import redis
 import time 
 
-def redis_pro(ran):
+def redis_pro(first, last):
     client = redis.Redis()
     with client.pipeline() as pipe:
-        for n in range(ran):
+        for n in range(first, last):
             pipe.set('key:'+str(n), 'value:'+str(n))
         pipe.execute()
 
 if __name__ == '__main__':
-    reg = 100_000
-    workers = 4
+    reg = 1_000_000
+    workers = 16
     pipe_width = 10_000
     start_time = time.time()
 
     print(f'Comenzando la carga de {reg:,} registros.')
     
     jobs = []
-    for i in range(1):
-        p = mp.Process(target=redis_pro, args=(100,))
+    for i in range(int((reg/pipe_width)+1)):
+        first = (i-1) * pipe_width
+        last = i * pipe_width
+        p = mp.Process(target=redis_pro, args=(first, last))
         jobs.append(p)
         p.start()
 
+    p.join()
     seconds = (time.time() - start_time)
     print(f'{reg:,} registros en: {seconds:.5} segundos\n')
