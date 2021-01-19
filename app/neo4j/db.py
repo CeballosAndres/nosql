1463eaf3cbd7c67dd2b36c6c50dc8d560df8ca54
José Andrés Ceballos Vadillo <andres@ceballos.dev>
2021-01-19 12:00:49 -0600

    add neo4j docker compose

---
+++ app/neo4j/db.py
@@ -0,0 +1,24 @@
+import redis
+from mysql import connector
+from cassandra.cluster import Cluster
+
+records = 10_000
+
+def redis_conn():
+	client = redis.Redis(
+		host = 'localhost',
+		port = 6379
+		)
+	return client
+
+def mysql_conn():
+	db_connection = connector.connect(
+		host='localhost',
+		user='root',
+		password='nosql',
+		database='test_db')
+	return db_connection
+
+def cassandra_conn():
+	cluster = Cluster(['0.0.0.0'], port=9042)
+	return cluster.connect('CityInfo', wait_for_all_pools=True)
